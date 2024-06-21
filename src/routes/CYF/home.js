cf22771acb246b36084ac5426f70382bae6a31f4
teerapongchaemchamrat <126034013+teerapongchaemchamrat@users.noreply.github.com>
2024-06-21 09:38:07 +0700

    Update home.js

--- src/routes/CYF/home.js
+++ src/routes/CYF/home.js
@@ -15,7 +15,7 @@ export default function Home (){
     const getData = async () => {
         try{
             setloading(true);
-            const getuser = await axios.get('http://192.168.10.114:88/cyf/user/result');
+            const getuser = await axios.get('http://XXXXXXXXXXXXXXXX');
             const userData = getuser.data.map(item => ({
               ...item,
               first_name : item.first_name || '',
